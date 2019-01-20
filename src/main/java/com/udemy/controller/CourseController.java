c5aae5136f76ec2d15a608b00cf8981da33e2ca1
Luis Monge <luismdam@gmail.com>
2019-01-20 13:09:47 +0100

    controller

--- src/main/java/com/udemy/controller/CourseController.java
+++ src/main/java/com/udemy/controller/CourseController.java
@@ -25,7 +25,7 @@ public class CourseController {
 	@Qualifier("courseServiceImpl")
 	private CourseService courseService;
 	
-	/ddd
+	//prueba
 	  
 	@GetMapping("/listcourses")
 	public ModelAndView listAllCourses() {
